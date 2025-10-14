f7a5f5ccb3e87df3c653d318f9500306eaa0cb71
rebartchekj <rebartchekj@msoe.edu>
2025-10-14 10:45:53 -0500

    test

--- src/coconuts/CocoLaserObserver.java
+++ src/coconuts/CocoLaserObserver.java
@@ -1,8 +1,10 @@
 package coconuts;
 
+import static coconuts.GameController.cocoDestroyedTextField;
+
 public class CocoLaserObserver implements Observer {
     @Override
     public void update() {
-
+        //test
     }
 }
--- src/coconuts/LaserBeam.java
+++ src/coconuts/LaserBeam.java
@@ -2,9 +2,12 @@ package coconuts;
 
 import javafx.scene.image.Image;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 // Represents the beam of light moving from the crab to a coconut; can hit only falling objects
 // This is a domain class; do not introduce JavaFX or other GUI components here
-public class LaserBeam extends IslandObject {
+public class LaserBeam extends HittableIslandObject {
     private static final int WIDTH = 2; // must be updated with image
     private static final Image laserImage = new Image("file:images/laser-1.png");
 
@@ -29,4 +32,5 @@ public class LaserBeam extends IslandObject {
         }
         return false;
     }
+
 }
