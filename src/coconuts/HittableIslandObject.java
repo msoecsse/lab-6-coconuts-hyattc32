4017c3701211caa9b480a291cf815eca6c5b5117
hyattc <hyattc@msoe.edu>
2025-10-14 02:28:10 -0500

    HittableIslandObject implements Subject - Chloe

--- src/coconuts/HittableIslandObject.java
+++ src/coconuts/HittableIslandObject.java
@@ -2,14 +2,41 @@ package coconuts;
 
 import javafx.scene.image.Image;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 // Represents island objects which can be hit
 // This is a domain class; do not introduce JavaFX or other GUI components here
-public abstract class HittableIslandObject extends IslandObject {
+public abstract class HittableIslandObject extends IslandObject implements Subject {
     public HittableIslandObject(OhCoconutsGameManager game, int x, int y, int width, Image image) {
         super(game, x, y, width, image);
     }
 
     public boolean isHittable() {
         return true;
+    }
+
+    protected Collection<Observer> observers = new LinkedList<Observer>();
+
+    @Override
+    public void attach(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer o : observers) {
+            o.update();
+        }
+    }
+
+    @Override
+    public boolean hasObserver(Observer o) {
+        return observers.contains(o);
     }
 }
